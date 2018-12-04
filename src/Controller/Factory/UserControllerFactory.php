dbf81d21a0a5073d6293fee5da83b615d7985c98
Masterflash <sxq@yandex.ru>
2018-12-04 11:28:29 +0300

    изменение соединения ADO

--- src/Controller/Factory/UserControllerFactory.php
+++ src/Controller/Factory/UserControllerFactory.php
@@ -15,7 +15,7 @@ class UserControllerFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $connection=$container->get('ADO\Connection');
+        $connection=$container->get('DefaultSystemDb');
         $config = $container->get('Config');
         $userManager = $container->get(UserManager::class);
         $translator = $container->get(TranslatorInterface::class);
