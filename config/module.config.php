24db83662b5d54ed4abb90276fb06518379141fd
Masterflash <sxq@yandex.ru>
2018-12-06 08:42:03 +0300

    полная переделка
    Не использовать!

--- config/module.config.php
+++ config/module.config.php
@@ -1,87 +1,22 @@
 <?php
 namespace Mf\Users;
 
-use Zend\Router\Http\Literal;
-use Zend\Router\Http\Segment;
-
 return [
-    'router' => [
-        'routes' => [
-            'login' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route'    => '/login',
-                    'defaults' => [
-                        'controller' => Controller\AuthController::class,
-                        'action'     => 'login',
-                    ],
-                ],
-            ],
-            'logout' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route'    => '/logout',
-                    'defaults' => [
-                        'controller' => Controller\AuthController::class,
-                        'action'     => 'logout',
-                    ],
-                ],
-            ],
-            'reset-password' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route'    => '/reset-password',
-                    'defaults' => [
-                        'controller' => Controller\UserController::class,
-                        'action'     => 'resetPassword',
-                    ],
-                ],
-            ],
-            'set-password' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route'    => '/set-password',
-                    'defaults' => [
-                        'controller' => Controller\UserController::class,
-                        'action'     => 'setPassword',
-                    ],
-                ],
-            ],
-            'users' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route'    => '/users[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id' => '[a-zA-Z0-9_-]*',
-                    ],
-                    'defaults' => [
-                        'controller'    => Controller\UserController::class,
-                        'action'        => 'index',
-                    ],
-                ],
-            ],
-        ],
-    ],
-    'controllers' => [
-        'factories' => [
-            Controller\AuthController::class => Controller\Factory\AuthControllerFactory::class,
-            Controller\UserController::class => Controller\Factory\UserControllerFactory::class,
-        ],
-    ],
-    'view_manager' => [
-        'template_path_stack' => [
-            __DIR__ . '/../view',
-        ],
-    ],
     "users" => [
-        "routeNameAfterLogin"=>"home"   //имя маршрута куда переходим после авторизации
-    ],
-    /*дополнение новым статусом статичные страницы*/
-    "statpage"=>[
-        'status'=>[
-            3=>"Различные оповещения и письма посетителям"
+        /*список допустимых состояний регистрированных юзеров, ключ - это код состояния*/
+        'users_status' => [
+            0=>"Неопределенное",
+            1=>"Неподтвержденная регистрация",
+            2=>"Ожидает подтверждения администрации",
+            3=>"Нормальное состояние",
+            4=>"Заблокирован",
         ],
+        /*код состояния при начальной регистрации нового посетителя*/
+        "users_status_start_registration" => 1,
+        /*новый код состояния после подтверждения регистрации*/
+        "users_status_after_confirm" => 3,
+        /*нормальное состояние посетителя, когда он может делать все*/
+        "users_status_normal" => 3,
     ],
   //обратный адрес
   "email_robot"=>"robot@".trim($_SERVER["SERVER_NAME"],"w."),
