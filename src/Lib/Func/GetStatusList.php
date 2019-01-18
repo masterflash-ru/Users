38abe8f61a42990ea4eb3f55d90043d6dd0a2d7c
Masterflash <sxq@yandex.ru>
2019-01-18 17:43:01 +0300

    fix config

--- src/Lib/Func/GetStatusList.php
+++ src/Lib/Func/GetStatusList.php
@@ -9,9 +9,10 @@ class GetStatusList
 function __invoke($obj,$infa,$struct_arr,$pole_type,$pole_dop,$tab_name,$idname,$const,$id,$action)
 
 {
+$status=$obj->config["users"]["users_status"];
+ksort($status);
 
-
-foreach ($obj->config["users"]["users_status"] as $k=>$status){
+foreach ($status as $k=>$status){
     $obj->dop_sql['name'][]=$status;
     $obj->dop_sql['id'][]=$k;
 
