b3742427505669cb43872bf5ef22c51179319997
Masterflash <sxq@yandex.ru>
2019-11-27 14:13:42 +0300

    Update Version20191104153553.php

--- migrations/Version20191104153553.php
+++ migrations/Version20191104153553.php
@@ -4,91 +4,154 @@ namespace Mf\Users;
 
 use Mf\Migrations\AbstractMigration;
 use Mf\Migrations\MigrationInterface;
+use Zend\Db\Sql\Ddl;
+use Zend\Db\Sql;
+
 
 class Version20191104153553 extends AbstractMigration implements MigrationInterface
 {
     public static $description = "Create users table";
 
-    public function up($schema)
+    public function up($schema, $adapter)
     {
-        switch ($this->db_type){
-            case "mysql":{
-                    $this->addSql("CREATE TABLE `users` (
-                      `id` int(11) NOT NULL AUTO_INCREMENT,
-                      `login` char(127) NOT NULL COMMENT 'логин, можно мыло',
-                      `status` int(11) NOT NULL COMMENT 'состояние юзера',
-                      `password` char(127) DEFAULT NULL COMMENT 'текущий пароль',
-                      `name` char(127) DEFAULT NULL COMMENT 'псевдоним',
-                      `full_name` char(255) DEFAULT NULL COMMENT 'ФИО',
-                      `temp_password` char(127) DEFAULT NULL COMMENT 'временный пароль для восстановления',
-                      `temp_date` datetime DEFAULT NULL COMMENT 'дата годности временного пароля для активации',
-                      `confirm_hash` char(50) DEFAULT NULL COMMENT 'строка для подтверждения регистрации',
-                      `date_registration` datetime DEFAULT NULL COMMENT 'дата регистрации',
-                      `date_last_login` datetime DEFAULT NULL COMMENT 'дата входа',
-                      PRIMARY KEY (`id`),
-                      KEY `temp_date` (`temp_date`),
-                      KEY `confirm_hash` (`confirm_hash`),
-                      KEY `status` (`status`),
-                      KEY `date_registration` (`date_registration`),
-                      KEY `login` (`login`)
-                    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='регистрированные юзеры (база)'");
+        $table = new Ddl\CreateTable("users");
+        $table->addColumn(new Ddl\Column\Integer('id',false,null,["AUTO_INCREMENT"=>true]));
+        $table->addColumn(new Ddl\Column\Char('login', 127,false,"",["COMMENT"=>"логин, можно мыло"]));
+        $table->addColumn(new Ddl\Column\Integer('status',false,0,["COMMENT"=>"состояние юзера"]));
+        $table->addColumn(new Ddl\Column\Char('password', 127,false,"",["COMMENT"=>"текущий пароль"]));
+        $table->addColumn(new Ddl\Column\Char('name', 127,false,"",["COMMENT"=>"псевдоним"]));
+        $table->addColumn(new Ddl\Column\Char('full_name', 255,false,"",["COMMENT"=>"ФИО"]));
+        $table->addColumn(new Ddl\Column\Char('temp_password', 127,true,"",["COMMENT"=>"временный пароль для восстановления"]));
+        $table->addColumn(new Ddl\Column\Datetime('temp_date', true,null,["COMMENT"=>"временный пароль для восстановления"]));
+        $table->addColumn(new Ddl\Column\Char('confirm_hash', 127,true,"",["COMMENT"=>"строка для подтверждения регистрации"]));
+        $table->addColumn(new Ddl\Column\Datetime('date_registration', true,null,["COMMENT"=>"дата регистрации"]));
+        $table->addColumn(new Ddl\Column\Datetime('date_last_login', true,null,["COMMENT"=>"дата входа"]));
+        $table->addConstraint(
+            new Ddl\Constraint\PrimaryKey(['id'])
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['temp_date'],"temp_date")
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['confirm_hash'],"confirm_hash")
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['status'],"status")
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['date_registration'],"date_registration")
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['login'],"login")
+        );
+        $this->mysql_add_create_table=" ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='регистрированные юзеры (база)'";
+        $this->addSql($table);
+        
+        $insert = new Sql\Insert("users");
+        $insert->columns(['id', 'login', 'status', 'password', 'name',"full_name"]);
+        $insert->values([1,"root",3,'$2y$10$R4b7CZhWutZPDlNFoUF9Se/LVeHrWtsnRj4OM4HxC4yxc3scLObYW',"root","root"]);
+        $this->addSql($insert);
+        $insert->values([2,"guest",4,'$2y$10$R4b7CZhWutZPDlNFoUF9Se/LVeHrWtsnRj4OM4HxC4yxc3scLObYW',"guest","guest"]);
+        $this->addSql($insert);
+        $insert->values([11,"test",4,'$2y$10$R4b7CZhWutZPDlNFoUF9Se/LVeHrWtsnRj4OM4HxC4yxc3scLObYW',"guest","guest"]);
+        $this->addSql($insert);
 
-                    $this->addSql('INSERT INTO `users` VALUES (1,"root",3,"$2y$10$R4b7CZhWutZPDlNFoUF9Se/LVeHrWtsnRj4OM4HxC4yxc3scLObYW","root","root",NULL,NULL,NULL,NULL,NULL)');
-                    $this->addSql('INSERT INTO `users` VALUES (2,"guest",4,"$2y$10$R4b7CZhWutZPDlNFoUF9Se/LVeHrWtsnRj4OM4HxC4yxc3scLObYW","guest","guest",NULL,NULL,NULL,NULL,NULL)');
-                    $this->addSql('INSERT INTO `users` VALUES (11,"test",4,"$2y$10$R4b7CZhWutZPDlNFoUF9Se/LVeHrWtsnRj4OM4HxC4yxc3scLObYW","guest","guest",NULL,NULL,NULL,NULL,NULL)');
+        $table = new Ddl\CreateTable("users_group");
+        $table->addColumn(new Ddl\Column\Integer('id',false,null,["AUTO_INCREMENT"=>true]));
+        $table->addColumn(new Ddl\Column\Char('name', 127,false,"",["COMMENT"=>"Имя группы"]));
+        $table->addColumn(new Ddl\Column\Char('description', 255,false,"",["COMMENT"=>"Описание"]));
+        $table->addConstraint(
+            new Ddl\Constraint\PrimaryKey(['id'])
+        );
+        $this->mysql_add_create_table=" ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Группы'";
+        $this->addSql($table);
 
-                    $this->addSql("CREATE TABLE `users_group` (
-                      `id` int(11) NOT NULL AUTO_INCREMENT,
-                      `name` char(127) DEFAULT NULL,
-                      `description` char(255) DEFAULT NULL,
-                      PRIMARY KEY (`id`)
-                    ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='Группы'");
-                    $this->addSql("INSERT INTO `users_group` VALUES (1,'Администраторы','Администратор - полный доступ ко всем ресурсам'),(2,'Гости','Гостевой вход - группа с минимальным уровнем доступа'),(6,'Редакторы','Редакторы информации'),(7,'Регистрированные посетители','Регистрированные посетители');");
+        $insert = new Sql\Insert("users_group");
+        $insert->columns(['id', 'name', 'description']);
+        $insert->values([1,'Администраторы','Администратор - полный доступ ко всем ресурсам']);
+        $this->addSql($insert);
+        $insert->values([2,'Гости','Гостевой вход - группа с минимальным уровнем доступа']);
+        $this->addSql($insert);
+        $insert->values([6,'Редакторы','Редакторы информации']);
+        $this->addSql($insert);
+        $insert->values([7,'Регистрированные посетители','Регистрированные посетители']);
+        $this->addSql($insert);
+        
+        $table = new Ddl\CreateTable("users2group");
+        $table->addColumn(new Ddl\Column\Integer('users',false));
+        $table->addColumn(new Ddl\Column\Integer('users_group',false));
+        $table->addConstraint(
+            new Ddl\Constraint\PrimaryKey(['users',"users_group"])
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['users'])
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(["users_group"])
+        );
+        $table->addConstraint(
+            new Ddl\Constraint\ForeignKey("users2group_fk","users","users","id","CASCADE")
+        );
+        $table->addConstraint(
+            new Ddl\Constraint\ForeignKey("users2group_fk1","users_group","users_group","id","CASCADE")
+        );
+        $this->mysql_add_create_table=" ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='юзер<->группа'";
+        $this->addSql($table);
 
+        $insert = new Sql\Insert("users2group");
+        $insert->columns(['users', 'users_group']);
+        $insert->values([1,1]);
+        $this->addSql($insert);
+        $insert->values([2,2]);
+        $this->addSql($insert);
+        $insert->values([11,2]);
+        $this->addSql($insert);
 
-                    $this->addSql("CREATE TABLE `users2group` (
-                      `users` int(11) NOT NULL,
-                      `users_group` int(11) NOT NULL,
-                      PRIMARY KEY (`users`,`users_group`),
-                      KEY `users` (`users`),
-                      KEY `users_group` (`users_group`),
-                      CONSTRAINT `users2group_fk` FOREIGN KEY (`users`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-                      CONSTRAINT `users2group_fk1` FOREIGN KEY (`users_group`) REFERENCES `users_group` (`id`) ON DELETE CASCADE
-                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='привязка юзер-группа'");
-                    $this->addSql("INSERT INTO `users2group` VALUES (1,1)");
-                    $this->addSql("INSERT INTO `users2group` VALUES (2,2)");
-                    $this->addSql("INSERT INTO `users2group` VALUES (11,2)");
+        $table = new Ddl\CreateTable("users_ext");
+        $table->addColumn(new Ddl\Column\Integer('id',false));
+        $table->addConstraint(
+            new Ddl\Constraint\PrimaryKey(['id'])
+        );
+        $table->addConstraint(
+            new Ddl\Constraint\ForeignKey("users_ext_fk","id","users","id","CASCADE")
+        );
+        $this->mysql_add_create_table=" ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='расширение юзеров'";
+        $this->addSql($table);
+        
+        $insert = new Sql\Insert("users_ext");
+        $insert->columns(['id']);
+        $insert->values([1]);
+        $this->addSql($insert);
+        $insert->values([2]);
+        $this->addSql($insert);
+        $insert->values([11]);
+        $this->addSql($insert);
 
-                    $this->addSql("CREATE TABLE `users_ext` (
-                      `id` int(11) NOT NULL,
-                      PRIMARY KEY (`id`),
-                      CONSTRAINT `users_ext_fk` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-                    ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8 COMMENT='расширение юзеров'");
-                    $this->addSql("INSERT INTO `users_ext` VALUES (1)");
-                    $this->addSql("INSERT INTO `users_ext` VALUES (2)");
-                    $this->addSql("INSERT INTO `users_ext` VALUES (11)");
+        $table = new Ddl\CreateTable("users_group_tree");
+        $table->addColumn(new Ddl\Column\Integer('id',false));
+        $table->addColumn(new Ddl\Column\Integer('parent_id',false));
+        
+        $table->addConstraint(
+            new Ddl\Constraint\PrimaryKey(['id',"parent_id"])
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['id'])
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['parent_id'])
+        );
+        $table->addConstraint(
+            new Ddl\Constraint\ForeignKey("users_group_tree_fk","id","users_group","id","CASCADE")
+        );
+        $table->addConstraint(
+            new Ddl\Constraint\ForeignKey("users_group_tree_fk1","parent_id","users_group","id","CASCADE")
+        );
+        $this->mysql_add_create_table=" ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='дерево групп'";
+        $this->addSql($table);
 
-
-
-                    $this->addSql("CREATE TABLE `users_group_tree` (
-                      `id` int(11) NOT NULL,
-                      `parent_id` int(11) NOT NULL,
-                      PRIMARY KEY (`id`,`parent_id`),
-                      KEY `role` (`id`),
-                      KEY `parent_role` (`parent_id`),
-                      CONSTRAINT `users_group_tree_fk` FOREIGN KEY (`id`) REFERENCES `users_group` (`id`) ON DELETE CASCADE,
-                      CONSTRAINT `users_group_tree_fk1` FOREIGN KEY (`parent_id`) REFERENCES `users_group` (`id`) ON DELETE CASCADE
-                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
-
-                break;
-            }
-            default:{
-                throw new \Exception("the database {$this->db_type} is not supported !");
-            }
-        }        
     }
 
-    public function down($schema)
+    public function down($schema, $adapter)
     {
         switch ($this->db_type){
             case "mysql":{
